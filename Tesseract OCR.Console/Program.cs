9cd1c3851b2974da4aa95f07402159cde7d70b00
Shiv Parakh Tyagi <shiv@imentor.co.in>
2018-02-20 16:22:36 +0530

    Tesseract OCR by Charles Weld 3.0.2

--- Tesseract OCR.Console/Program.cs
+++ Tesseract OCR.Console/Program.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Tesseract_OCR.Console
+﻿namespace Tesseract_OCR.Console
 {
     class Program
     {
